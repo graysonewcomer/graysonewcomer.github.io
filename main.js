fcb01dfc10be6aa42ce52bc9f981021e87fc69d1
Grayson Newcomer <grayson.newcomer@gmail.com>
2022-07-31 10:28:51 -0500

    Actually adding 3D

---
+++ main.js
@@ -0,0 +1,24 @@
+import './app.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas : document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+
+const geometry = new THREE.BoxGeometry(10, 10, 10,);
+const material = new THREE.MeshBasicMaterial({color: green, wireframe: true});
+const box = new THREE.Mesh(geometry, material);
+
+scene.add(box);
