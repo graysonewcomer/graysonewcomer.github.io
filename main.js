01ff703351abfca189936ad0334a9c05df5eac08
Grayson Newcomer <grayson.newcomer@gmail.com>
2022-07-31 17:53:55 -0500

    added more to directory

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-import * as THREE from './node_modules/three';
+import * as THREE from './node_modules/three/build/three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
