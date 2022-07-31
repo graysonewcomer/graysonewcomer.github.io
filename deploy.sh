2c2b2bc71b19ec3fad3d44fa69ac2e077b04a51e
Grayson Newcomer <grayson.newcomer@gmail.com>
2022-07-31 17:43:00 -0500

    Added deploy.sh, viteconfig

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git checkout -b main
+git add -A 
+git commit -m 'deploy'
+
+git push -f git@github.com:graysonewcomer/graysonewcomer.github.io main
+
+cd -
